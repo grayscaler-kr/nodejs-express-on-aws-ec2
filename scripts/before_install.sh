ecc563981e34614e3a3871aa9c39da8078ee31c4
grayscaler <wishcome@gmail.com>
2025-02-14 17:21:36 +0900

    v2:node 18

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,7 +6,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 # nvm install node
 nvm install 16
 nvm install 18.17.1
-nvm use 16
+# nvm use 16
+nvm use 18.17.1
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
